96330efabed120fd87a8bbf79e20b20c0528c852
Luis Ruediger <luisruediger27@gmail.com>
2023-02-01 06:46:39 -0300

    post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from './components/Header'
-
-import styles from './App.module.css'
-import './global.css'
-
 import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
+
+import './global.css'
+import styles from './App.module.css'
+
 
 function App() {
   return (
@@ -12,9 +13,9 @@ function App() {
 
       <div className={styles.wrapper}>
        <Sidebar />
-
         <main>
-          posts
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
