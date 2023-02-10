92ef62489028e5244a7bfc2d6a0d8f0c5989d077
Luis Ruediger <luisruediger27@gmail.com>
2023-02-10 11:28:09 -0300

    iterando no JSX / Propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,42 @@ import { Post } from './components/Post'
 import './global.css'
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/luisruediger.png',
+      name: 'Luis Ruediger',
+      role: 'Desenvolvedor front-end',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+
+      { type: 'link', content: 'jane.design/doctorcare' },
+
+    ],
+    publishedAt: new Date('2023-02-01 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+
+      { type: 'link', content: 'jane.design/doctorcare' },
+
+    ],
+    publishedAt: new Date('2023-02-09 20:00:00')
+  },
+]
 
 function App() {
   return (
@@ -14,8 +50,16 @@ function App() {
       <div className={styles.wrapper}>
        <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              /> 
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,35 +1,43 @@
+import { format, formatDistanceToNow, formatISO } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+  const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'ás' HH:mm'h'", { locale: ptBR })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { addSuffix: true, locale: ptBR })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/diego3g.png" />
+          <Avatar src={author.avatarUrl} />
         
           <div className={styles.authorInfo}>
-            <strong>Luis Ruediger</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title='01 de fevereiro ás 06:30h' dateTime="2023-02-01 06:30:00">Publicado há 1h</time>
+        <time 
+          title={publishedDateFormatted} 
+          dateTime={formatISO(publishedAt)}
+        >
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-      <p>Fala galeraa 👋</p>
-
-      <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-      <p><a href=''>jane.design/doctorcare</a></p>
-
-      <p>
-        <a href=''>#novoprojeto </a>{' '}
-        <a href=''>#nlw </a>{' '}
-        <a href=''>#rocketseat</a>
-      </p>
+        {content.map(item => {
+          if (item.type === 'paragraph') {
+            return <p>{item.content}</p>
+          } else if (item.type === 'link') {
+            return <p><a href="#">{item.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
